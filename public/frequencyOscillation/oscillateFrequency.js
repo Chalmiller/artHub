b97c9ab9fd1f3a23357d91329edf955a062a5fe0
Christopher Miller <christopher@transcriptic.com>
2017-08-24 12:59:14 -0700

    Changes to the functions

--- public/frequencyOscillation/oscillateFrequency.js
+++ public/frequencyOscillation/oscillateFrequency.js
@@ -11,11 +11,11 @@ function setup() {
 }
 
 function draw() {
-  background(255);
+  background(100);
 
   var waveform = fft.waveform();  // analyze the waveform
   beginShape();
-  strokeWeight(5);
+  strokeWeight(10);
   for (var i = 0; i < waveform.length; i++){
     var x = map(i, 0, waveform.length, 0, width);
     var y = map(waveform[i], -1, 1, height, 0);
@@ -24,9 +24,9 @@ function draw() {
   endShape();
 
   // change oscillator frequency based on mouseX
-  var freq = map(mouseX, 0, width, 40, 880);
+  var freq = map(mouseX, 0, width, 100, 1000);
   osc.freq(freq);
 
-  var amp = map(mouseY, 0, height, 1, .01);
+  var amp = map(mouseY, 0, height, 5, .01);
   osc.amp(amp);
 }
