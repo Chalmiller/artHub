f5858b26c424ddd915b295296f34b5ac697b48b6
ArbutusNate <arbutusnate@gmail.com>
2017-08-25 12:53:31 -0700

    still tweaking connection stuff, trying to make it work on both local and heroku

--- config/connection.js
+++ config/connection.js
@@ -1,15 +1,15 @@
 //Almost certainly can remove the jawsDB part of this but leaving it until we settle on storage.
 
 var mysql = require('mysql');
-// var keys = require('./apikey/keys.js');
 
-// var sqlConnect = require("./apikeys/keys.js");
+
 // Mysql
 
 if (process.env.JAWSDB_URL) {
   connection = mysql.createConnection(process.env.JAWSDB_URL);
 } else {
-  // var connection = mysql.createConnection(keys.sqlConnect);
+  var keys = require('./apikey/keys.js');
+  var connection = mysql.createConnection(keys.sqlConnect);
 }
 
 
